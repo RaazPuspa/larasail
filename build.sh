4d2719e0e9eee92aac5b7293cf6b553597bd56c9
Pusparaj <10107534+RaazPuspa@users.noreply.github.com>
2023-01-20 15:47:03 +0545

    Prepare bash script to build and publish images for given versions

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+LATEST=false
+VERSIONS=('7.3' '7.4' '8.0' '8.1' '8.2')
+
+while [ ! $# -eq 0 ]; do
+    case "$1" in
+    -l | --latest)
+        LATEST=true
+        ;;
+    -v | --version)
+        if [ "$2" ]; then
+            VERSIONS=($2)
+            shift
+        else
+            echo "${1} requires a value"
+            exit 1
+        fi
+        ;;
+    esac
+
+    shift
+done
+
+for VERSION in "${VERSIONS[@]}"
+do
+  docker build larasail --no-cache --build-arg PHP_VERSION=$VERSION -t raazpuspa/larasail:$VERSION --target=default
+  docker push raazpuspa/larasail:$VERSION
+
+  docker build larasail --build-arg PHP_VERSION=$VERSION -t raazpuspa/larasail:$VERSION-wkhtml --target=wkhtml
+  docker push raazpuspa/larasail:$VERSION-wkhtml
+done
+
+if [ "$LATEST" = true ]; then
+  docker tag raazpuspa/larasail:$VERSION raazpuspa/larasail:latest
+  docker push raazpuspa/larasail:latest
+
+  docker tag raazpuspa/larasail:$VERSION-wkhtml raazpuspa/larasail:latest-wkhtml
+  docker push raazpuspa/larasail:latest-wkhtml
+fi
